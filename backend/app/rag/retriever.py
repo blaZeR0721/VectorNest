a53502d9f86b53f832202e1f2c8557948cbec125
blaZeR0721 <vpatel1243@gmail.com>
2026-04-04 18:19:50 +0530

    updated

--- backend/app/rag/retriever.py
+++ backend/app/rag/retriever.py
@@ -3,17 +3,25 @@ from pinecone_text.sparse import BM25Encoder
 from app.ingestion.embedder import get_embeddings
 from app.db.pinecone_client import index
 from functools import lru_cache
+import os
+
+BM25_PATH = os.path.join(os.path.dirname(__file__), "bm25_default.json")
 
 @lru_cache(maxsize=1)
 def _get_bm25() -> BM25Encoder:
-    return BM25Encoder.default()
+    if os.path.exists(BM25_PATH):
+        return BM25Encoder().load(BM25_PATH)
+    bm25 = BM25Encoder.default()
+    bm25.dump(BM25_PATH)
+    return bm25
 
+@lru_cache(maxsize=1)
 def get_retriever():
     return PineconeHybridSearchRetriever(
         embeddings=get_embeddings(),
         sparse_encoder=_get_bm25(),
         index=index,
         top_k=4,
-        alpha=0.5,
+        alpha=0.6,
         text_key="text",
     )
